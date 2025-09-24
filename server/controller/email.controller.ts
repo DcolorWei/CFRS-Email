4630988fe4f6c9071d5665357b70ce9bf38744cb
DcolorWei <corfer@yeah.net>
2025-09-24 15:21:51 +0800

    feature: get otp and url from email text

--- server/controller/email.controller.ts
+++ server/controller/email.controller.ts
@@ -1,9 +1,9 @@
 import { EmailImpl } from "../../shared/impl";
-import { EmailListQuery, EmailListResponse, EmailRouterInstance, EmailSenderBody, EmailSenderResponse, EmailWebsocketInstance } from "../../shared/router/EmailRouter";
+import { EmailListQuery, EmailListResponse, EmailOtpQuery, EmailOtpResponse, EmailRouterInstance, EmailSenderBody, EmailSenderResponse, EmailUrlQuery, EmailUrlResponse, EmailWebsocketInstance } from "../../shared/router/EmailRouter";
 import { inject, injectws } from "../lib/inject";
 import { verifytoken } from "../service/auth.service";
 import { sendEmail } from "../service/email.send";
-import { getEmailList } from "../service/email.service";
+import { getEmailList, getRecentOtp, getRecentUrls } from "../service/email.service";
 
 async function queryEmailList(query: EmailListQuery): Promise<EmailListResponse> {
     if (!query.auth || !query.page) {
@@ -23,10 +23,20 @@ async function queryEmailList(query: EmailListQuery): Promise<EmailListResponse>
     return result;
 }
 
+async function requestOtp(query: EmailOtpQuery): Promise<EmailOtpResponse> {
+    const otp = await getRecentOtp(query.email) || "";
+    return { otp };
+}
+
+async function requestUrl(query: EmailUrlQuery): Promise<EmailUrlResponse> {
+    const url = await getRecentUrls(query.email, query.keyword) || "";
+    return { url };
+}
+
 async function requestSendMail(body: EmailSenderBody): Promise<EmailSenderResponse> {
     const success = await sendEmail(body);
     return { success };
 }
 
-export const emailController = new EmailRouterInstance(inject, { queryEmailList, requestSendMail });
+export const emailController = new EmailRouterInstance(inject, { queryEmailList, requestOtp, requestUrl, requestSendMail });
 export const emailWSController = new EmailWebsocketInstance(injectws, { queryEmailList });
--- server/service/email.service.ts
+++ server/service/email.service.ts
@@ -12,22 +12,48 @@ const strategyRepository = Repository.instance(StrategyEntity);
 
 
 export async function getEmailList(account: string): Promise<EmailEntity[]> {
-    const inboxSet = new Set(strategyRepository.find({ creater: account }).map(item => item.email));
-    const emailList = emailRepository.find();
+    const inboxSet = new Set((await strategyRepository.find({ creater: account })).map(item => item.email));
+    const emailList = await emailRepository.find();
     const result = emailList.filter(({ to }) => inboxSet.has(to));
     return result;
+}
+
+export async function getRecentOtp(email: string): Promise<string | null> {
+    const emails = await emailRepository.find({ to: email });
+    const recentEmail = emails.reduce((prev, curr) => {
+        if (curr.time > prev.time) return curr
+        return prev;
+    });
+    if (Date.now() - recentEmail.create_time < 60 * 1000) {
+        const match = recentEmail.text.match(/\b\d{4,6}\b/);
+        if (match) return match[0];
+    }
+    return null;
+}
+export async function getRecentUrls(email: string, filterStr: string): Promise<string | null> {
+    const emails = await emailRepository.find({ to: email });
+    const recentEmail = emails.reduce((prev, curr) => {
+        if (curr.time > prev.time) return curr
+        return prev;
+    });
+    if (Date.now() - recentEmail.create_time < 60 * 1000) {
+        const match = recentEmail.text.match(/(https?:\/\/[^\s]+)/g) || [];
+        const filteredUrls = match.filter(url => url.includes(filterStr));
+        return filteredUrls[0];
+    }
+    return null;
 }
 
 export async function saveReceivedEmail(emails: Array<EmailImpl>): Promise<boolean> {
     const wsService = WebSocketServerService.getInstance();
     const result = emailRepository.insertMany(emails);
-    emails.forEach((item) => {
-        const strategy = strategyRepository.find({ email: item.to });
+    for (const item of emails) {
+        const strategy = await strategyRepository.find({ email: item.to });
         strategy.forEach((sitem => {
             const starthtml = `<p>发件人: ${item.from}</p><p>收件人: ${item.to}</p><p>原文：</p>`
             sendEmail({ to: sitem.forward, subject: item.subject, html: starthtml + item.html });
         }))
-    })
+    }
     wsService.triggerEvent("queryEmailList", JSON.stringify(emails));
     return result;
 }
